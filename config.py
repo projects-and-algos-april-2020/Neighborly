b14ee31be22b00047bd32d097ac95ffcbeee06f2
James Traub <traub01-devtrain@outlook.com>
2020-05-07 02:28:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 import urllib.request
-<<<<<<< HEAD
-UPLOAD_FOLDER = '\\static\\'
-=======
 UPLOAD_FOLDER = 'C:\\Users\\imeli\\OneDrive\\Documents\\CodingDojo\\Projects_Algos\\group_project\\static\\'
-
->>>>>>> 1560abe593965f9581040dc7495c562a15265dad
-
 
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///neighborly.db'
