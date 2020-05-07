25bc51dbbae67845a40336d95c276a6d8a51511d
flyin-wheels <traub01-devtrain@outlook.com>
2020-05-07 01:29:24 -0700

    making changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 import urllib.request
-UPLOAD_FOLDER = 'C:\\Users\\imeli\\OneDrive\\Documents\\CodingDojo\\Projects_Algos\\group_project\\static\\'
+UPLOAD_FOLDER = 'C:\\Users\\traubhome\\Desktop\\Coding_Dojo\\Projects and Algorithms\\Neighborly\\static'
 
 
 
